a4cda6bb2e580760fd89dee4b55f3185d37d6a5b
manahara dissanayake <manaharadissanayake@gmail.com>
2025-08-17 21:29:56 +0530

    Added folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Manahara Dissanayake',
+    author_email= 'manaharadissanayake@gmail.com',
+    #find __init__.py if its there it will be considered as the local file
+    packages= find_packages(),
+    install_requires = []
+)
